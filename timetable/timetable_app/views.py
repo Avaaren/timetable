128e13ce0192cd46a3aa8db50f7da5bf401688fa
Avaaren <avaaren@mail.ru>
2020-06-21 13:05:34 +0300

    Added APIView for timetable list

--- timetable/timetable_app/views.py
+++ timetable/timetable_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .models import Timetable
+from .serializers import TimetableSerializer
 
-# Create your views here.
+
+class GetTimetableView(ListAPIView):
+
+    queryset = Timetable.objects.all()
+    serializer_class = TimetableSerializer
+
