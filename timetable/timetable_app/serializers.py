e1545d9aee662c44a4d6848a54b5609498112031
Avaaren <avaaren@mail.ru>
2020-06-21 13:15:17 +0300

    Added serializers for all models and timetable serializer now has nested relations with other models

--- timetable/timetable_app/serializers.py
+++ timetable/timetable_app/serializers.py
@@ -1,10 +1,42 @@
 from rest_framework import serializers
 from .models import (
-    Timetable
+    Timetable,
+    Group,
+    Day,
+    Class,
+    Cabinet
 )
 
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = ['number',]
+
+class DaySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Day
+        fields = ['day', 'day_of_week']
+
+class ClassSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Class
+        fields = ['name',]
+
+class CabinetSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Cabinet
+        fields = ['number', 'is_free']
+
 class TimetableSerializer(serializers.ModelSerializer):
+    group = GroupSerializer(read_only=True)
+    day = DaySerializer(read_only=True)
+    name_of_class = ClassSerializer(read_only=True)
+    cabinet = CabinetSerializer(read_only=True)
 
     class Meta:
         model = Timetable
-        fields = '__all__'
+        fields = ['id', 'number_of_class', 'group', 'day', 'name_of_class', 'cabinet']
