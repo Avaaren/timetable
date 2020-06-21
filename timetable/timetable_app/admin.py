4a0ee2ccccea9ee2c835962b5640d57642130b5f
Avaaren <avaaren@mail.ru>
2020-06-21 13:04:53 +0300

    Registered all models on admin site

--- timetable/timetable_app/admin.py
+++ timetable/timetable_app/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import (
+    Timetable,
+    Group,
+    Day,
+    Class, 
+    Cabinet
+)
 
-# Register your models here.
+
+@admin.register(Timetable)
+class TimetableAdmin(admin.ModelAdmin):
+    
+    class Meta:
+        list_display = '__all__'
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    
+    class Meta:
+        list_display = ['name',]
+        list_filter = ['name',]
+
+@admin.register(Cabinet)
+class TimetableAdmin(admin.ModelAdmin):
+    
+    class Meta:
+        list_display = ['number', 'is_free']
+        list_filter = ['number', 'is_free']
+
+@admin.register(Day)
+class TimetableAdmin(admin.ModelAdmin):
+    
+    class Meta:
+        list_display = ['day', 'day_of_week']
+        list_filter = ['day', 'day_of_week']
+
+@admin.register(Class)
+class TimetableAdmin(admin.ModelAdmin):
+    
+    class Meta:
+        list_display = ['name',]
+        list_filter = ['name',]
