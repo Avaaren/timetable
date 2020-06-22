1e2bee611f5596214206eb8c3b5f849c3c820670
Avaaren <avaaren@mail.ru>
2020-06-22 15:34:33 +0300

    Updated timetable serializer with create timetable method

--- timetable/timetable_app/serializers.py
+++ timetable/timetable_app/serializers.py
@@ -32,11 +32,23 @@ class CabinetSerializer(serializers.ModelSerializer):
         fields = ['number', 'is_free']
 
 class TimetableSerializer(serializers.ModelSerializer):
-    group = GroupSerializer(read_only=True)
-    day = DaySerializer(read_only=True)
-    name_of_class = ClassSerializer(read_only=True)
-    cabinet = CabinetSerializer(read_only=True)
+    group = GroupSerializer()
+    day = DaySerializer()
+    name_of_class = ClassSerializer()
+    cabinet = CabinetSerializer()
 
     class Meta:
         model = Timetable
         fields = ['id', 'number_of_class', 'group', 'day', 'name_of_class', 'cabinet']
+    # Объект создается. Сделать проверку существование get() полей. Сделайть чтобы при занятии кабинета
+    # в какой либо из дней нельзя было его на этот же день поставить
+    def create(self, validated_data):
+        group = Group.objects.get(number=validated_data['group']['number'])
+        day = validated_data.pop('day')
+        day = Day.objects.get_or_create(day=day['day'], day_of_week=day['day_of_week'])
+        cabinet = Cabinet.objects.get(number=validated_data['cabinet']['number'])
+        name_of_class = Class.objects.get(name=validated_data['name_of_class']['name'])
+        number_of_class = validated_data['number_of_class']
+        timetable = Timetable.objects.create(group=group, day=day[0], cabinet=cabinet, name_of_class=name_of_class, number_of_class=number_of_class)
+        
+        return timetable
