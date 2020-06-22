f3a4e218a024efaa554d450e9c2eabff2c41a09a
Avaaren <avaaren@mail.ru>
2020-06-22 13:59:18 +0300

    updated urls. Added timetable by group url

--- timetable/timetable_app/urls.py
+++ timetable/timetable_app/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.GetTimetableView.as_view(), name='general_timetable')
-    # path('<slug:group>', views.GroupTimetableView.as_view, name='group_timetable')
-    # path('add', views.AddTimetableView.as_view, name='add_timetable')
-    # path('edit', views.EditTimetableView.as_view, name='edit_timetable')
-    # path('delete', views.DeleteTimetableView.as_view, name='edit_timetable')
+    path('', views.GetTimetableView.as_view(), name='general_timetable'),
+    path('<slug:group>/', views.GroupTimetableView.as_view(), name='group_timetable'),
+    # path('add', views.AddTimetableView.as_view, name='add_timetable'),
+    # path('edit', views.EditTimetableView.as_view, name='edit_timetable'),
+    # path('delete', views.DeleteTimetableView.as_view, name='edit_timetable'),
 
 ]
