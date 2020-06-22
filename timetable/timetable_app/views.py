99d1a10d75c18d0b4cc61f53b62f86d3bc14a3a8
Avaaren <avaaren@mail.ru>
2020-06-22 13:59:50 +0300

    Added view for displaying timetable by group

--- timetable/timetable_app/views.py
+++ timetable/timetable_app/views.py
@@ -1,5 +1,13 @@
-from rest_framework.generics import ListAPIView
-from .models import Timetable
+from django.shortcuts import get_object_or_404
+
+from rest_framework.generics import (
+    ListAPIView,
+    RetrieveUpdateDestroyAPIView
+)
+from .models import (
+    Timetable,
+    Group
+)
 from .serializers import TimetableSerializer
 
 
@@ -8,3 +16,12 @@ class GetTimetableView(ListAPIView):
     queryset = Timetable.objects.all()
     serializer_class = TimetableSerializer
 
+
+class GroupTimetableView(ListAPIView):
+
+    serializer_class = TimetableSerializer
+
+    def get_queryset(self):
+        group = self.kwargs.get('group')
+        group = get_object_or_404(Group, slug=group)
+        return Timetable.objects.filter(group=group)
