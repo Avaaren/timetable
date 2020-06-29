79b1bd9b5eb470036e553b71ac4b7ca1ce0f39e1
Avaaren <avaaren@mail.ru>
2020-06-29 13:13:44 +0300

    little fix

--- frontend/timetable/src/services/TimetableService.js
+++ frontend/timetable/src/services/TimetableService.js
@@ -14,12 +14,13 @@ export default class TimetableService{
         const url = `${API_URL}/api/timetable/${group_slug}`;
         return axios.get(url).then(response => response.data);
     }
-    getTimetable(pk) {
-        const url = `${API_URL}/api/timetable/${pk}/edit/`;
+    getTimetable(id) {
+        const url = `${API_URL}/api/timetable/${id}/edit/`;
         return axios.get(url).then(response => response.data);
     }
-    deleteTimetable(timetable){
-        const url = `${API_URL}/api/timetable/${timetable.pk}/delete/`;
+    deleteTimetable(id){
+        console.log(id);
+        const url = `${API_URL}/api/timetable/${id}/delete/`;
         return axios.delete(url);
     }
     createTimetable(timetable){
@@ -27,7 +28,7 @@ export default class TimetableService{
         return axios.post(url,timetable);
     }
     updateTimetable(timetable){
-        const url = `${API_URL}/api/timetable/${timetable.pk}/edit/`;
+        const url = `${API_URL}/api/timetable/${timetable.id}/edit/`;
         return axios.put(url,timetable);
     }
 }
