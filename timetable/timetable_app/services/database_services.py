eda3f7d2474c94d8d213cba89c24f375a355947e
Avaaren <avaaren@mail.ru>
2020-06-29 11:46:50 +0300

    Added method for check is cabinet free

--- timetable/timetable_app/services/database_services.py
+++ timetable/timetable_app/services/database_services.py
@@ -1,4 +1,5 @@
-from timetable_app.models import Day
+from timetable_app.models import Day, Timetable
+from rest_framework import serializers
 
 import dateutil.parser as parser
 from datetime import datetime
@@ -10,4 +11,11 @@ def validate_day(day):
     weekday = day.weekday()
     if not Day.objects.filter(day=day).exists():
         Day.objects.create(day=day, day_of_week=weekday)
+
+def check_cabinet(cabinet, day, number_of_class):
+    '''Checking is cabinet free at selected date and number of class'''
+    if Timetable.objects.filter(day=day, cabinet=cabinet, number_of_class=number_of_class).exists():
+        raise serializers.ValidationError("Cabinet is not free")
+    else:
+        return True
         
