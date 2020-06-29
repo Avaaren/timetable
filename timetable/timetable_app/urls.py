26a4b4c157e219442bd456233ddc4f0a87cc1e1a
Avaaren <avaaren@mail.ru>
2020-06-29 10:19:43 +0300

    Fixed create timetable error

--- timetable/timetable_app/urls.py
+++ timetable/timetable_app/urls.py
@@ -2,10 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Timetable urls
     path('', views.GetTimetableView.as_view(), name='general_timetable'),
-    path('<slug:group>/', views.GroupTimetableView.as_view(), name='group_timetable'),
     path('add/', views.AddTimetableView.as_view(), name='add_timetable'),
+    path('<slug:group>/', views.GroupTimetableView.as_view(), name='group_timetable'),
     path('<int:pk>/edit/', views.EditTimetableView.as_view(), name='edit_timetable'),
     path('<int:pk>/delete/', views.DeleteTimetableView.as_view(), name='edit_timetable'),
+
+    # Group urls
+
+    # path()
 
 ]
